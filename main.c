8979ec43e254dc22e9ebadc2445173005eface60
unknown <praveenrajm82@gmail.com>
2024-10-26 14:55:17 +0530

    SIVACode

---
+++ main.c
@@ -0,0 +1,30 @@
+
+
+#include <stdio.h>
+#include <studio.c>
+#include <stdio.h>
+
+void main()
+{
+    int i=6;
+for (i=6;i<=13;i++)
+{
+    if(i%==0)
+    {
+    printf("even: %d\n\tStatic: %d\n", i);
+    }
+    printf("Loop outer: %d\n\tStatic: %d\n", i);
+
+    else if(i%!=0)
+    {
+        printf("Odd: %d\n\tStatic: %d\n", i);
+    }
+    else()
+    {
+          printf("Odd: %d\n\tStatic: %d\n", i);
+    }
+    }
+  }
+
+}
+
